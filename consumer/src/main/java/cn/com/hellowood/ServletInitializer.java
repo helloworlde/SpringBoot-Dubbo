ce450b55161c17591e0355810e45bfcf386ae760
何惠民 <huimin.he@yi23.net>
2017-11-01 23:51:22 +0800

    Change to generate war package

---
+++ consumer/src/main/java/cn/com/hellowood/ServletInitializer.java
@@ -0,0 +1,15 @@
+package cn.com.hellowood;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * @author HelloWood
+ **/
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ConsumerApplication.class);
+    }
+}
